8f1a6d1b14b2c8cc65ce0372ab2ca8617270bf10
Maëva Trivino <maevatri@aol.com>
2020-02-10 17:28:40 -0500

    :lipstick: style homeTab

--- tuto_react/src/components/tabs/homeTab.js
+++ tuto_react/src/components/tabs/homeTab.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react';
-import {Text, View, ScrollView} from "react-native";
+import {Text, View, ScrollView, StyleSheet} from "react-native";
 
 export default class HomeTab extends Component{
     static homeView (){
         return(
-            <ScrollView>
-                <Text>HomeTabView</Text>
-            </ScrollView>
+            <View style={styleHome.container}>
+                <View style={styleHome.titleContainer}>
+                    <Text style={styleHome.title}>Welcome ...</Text>
+                </View>
+            </View>
         );
     }
 }
+
+const styleHome = StyleSheet.create({
+    container:{
+        flex: 1,
+        justifyContent: 'center',
+    },
+    titleContainer:{
+        margin: 5,
+        alignItems: 'center',
+    },
+    title: {
+        fontSize: 30
+    }
+});
+

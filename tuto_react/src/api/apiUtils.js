cf6db181c7af49e8c00a6f3f7c2638ae0932d6d0
Fab <fab.mckay@gmail.com>
2020-02-13 00:00:12 -0500

    :construction: Search (API + Parsing)

--- tuto_react/src/api/apiUtils.js
+++ tuto_react/src/api/apiUtils.js
@@ -13,13 +13,20 @@ export const getUserPlaylists = async() =>
 {
     const apiWrapper = await getAPIWrapper();
     const apiResponse = await apiWrapper.getUserPlaylists();
-
     let parsingStrategy = getParsingStrategy(parsingTypes.PLAYLISTS_LIST);
-
     let returnObject = parsingStrategy(apiResponse);
+    return returnObject;
+}
 
+export const search = async(query) =>
+{
+    const apiWrapper = await getAPIWrapper();
+
+    const searchTypes = ["album","artist","playlist","track"];
+    const apiResponse = await apiWrapper.search(query,searchTypes);
+    let parsingStrategy = getParsingStrategy(parsingTypes.SEARCH);
+    let returnObject = parsingStrategy(apiResponse);
     console.log(JSON.stringify(returnObject));
-
     return returnObject;
 }
 
